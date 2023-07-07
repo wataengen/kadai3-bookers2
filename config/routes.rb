35198779541dd9e302cac2b485b935ce2ef28c2a
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-07-07 08:48:18 +0000

    [Finish]viweファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'homes/index'
+  get 'homes/show'
   devise_for :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
