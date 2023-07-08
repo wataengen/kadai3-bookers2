c77d3c69b66d0783940da04d3d9868508d758005
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-07-08 02:51:18 +0000

    [Update]ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/index'
-  get 'homes/show'
+  root to: 'homes#index'
   devise_for :users
+  resources :homes, only: [:index, :about, :show]
+  resources :books, only: [:new, :edit, :show, :index]
+
+  #get 'users/index'
+  #get 'users/show'
+  #get 'users/edit'
+  #get 'books/new'
+  #get 'books/index'
+  #get 'books/show'
+  #get 'books/edit'
+  #get 'homes/index'
+  #get 'homes/show'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
