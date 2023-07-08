1cb613bc24ffb5c5169747d30c122aadcf998808
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-07-08 10:49:42 +0000

    [Update]データベースに外部キーを追加

--- db/migrate/20230707073024_create_books.rb
+++ db/migrate/20230707073024_create_books.rb
@@ -5,6 +5,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
 
       t.string :title
       t.text :opinion
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2023_07_07_073024) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "opinion"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
